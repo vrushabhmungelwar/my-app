a3dd37f6a7673422113b94a2abc385c4cae81743
vrushabhmungelwar <vrushabhmungelwar10@gmail.com>
2021-11-01 17:08:51 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
+
+// export default function App() {
+//   console.log("Hi");
+//   return (
+//     <div className="App">
+//       <Msg name="Ajith" />
+//       <Msg name="Ajith" />
+//       <Msg name="Ajith" />
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   console.log("Hi");
+//   const names = ["Ajith", "Kewal", "Deepa", "Robin"]
+//   return (
+//      <div className="App">
+//         {names.map(nm=> <Msg name = {nm} />)}
+//      </div>
+//   );
+// }
+// function Msg({ name }) {
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//     </div>
+//   );
+// }
+
+export default function App() {
+  console.log("Hi");
+  const users = [
+    {
+      name: "Ajith",
+      pic: "https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/05/Whatsapp-Profile-Images-wallpaper-hd-300x300.gif",
+    },
+    {
+      name: "Kewal",
+      pic: "https://www.seekpng.com/png/detail/506-5061704_cool-profile-avatar-picture-cool-picture-for-profile.png",
+    },
+    {
+      name: "Deepa",
+      pic: "https://cdn.pixabay.com/photo/2015/01/06/16/14/woman-590490__340.jpg",
+    },
+    {
+      name: "Robin",
+      pic: "https://st2.depositphotos.com/1531183/5770/v/950/depositphotos_57709697-stock-illustration-male-person-silhouette-profile-picture.jpg",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((ur) => (
+        <Msg name={ur.name} pic={ur.pic} />
+      ))}
+    </div>
+  );
+}
+function Msg({ name, pic }) {
+  return (
+    <div>
+      <img className="user-pic" src={pic} alt={name} />
+      <h1 className="user-name">{name}</h1>
     </div>
   );
 }
 
-export default App;
